155d281b4f1fc1e71ecc9437b5afa19c18aa25d9
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-09-16 17:11:49 -0400

    Third Heroku

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,6 @@
-//home.ts
 import {Component} from "@angular/core";
-import {ItemSliding, Item} from 'ionic-angular';
+import {NavController, ItemSliding, Item} from 'ionic-angular';
+import {FriendEditPage} from '../friend-edit/friend-edit';
 import {FriendService} from '../../providers/friend-service/friend-service';
 import {Friend} from '../../friend.ts';
 
@@ -9,16 +9,47 @@ import {Friend} from '../../friend.ts';
   providers: [FriendService]
 })
 export class HomePage {
-public friends: Friend[];
+  public friends: Friend[];
 
-constructor(public friendService: FriendService) {
-  this.loadFriends();
-}
+  constructor(public friendService: FriendService,
+              public nav: NavController) {
+    this.loadFriends();
+  }
 
-loadFriends() {
-  this.friendService.load()
-      .subscribe(data => {
-        this.friends = data;
+  loadFriends() {
+    this.friendService.load()
+      .subscribe(friendList => {
+        this.friends = friendList;
       })
+  }
+
+  addFriend(friend:string) {
+    this.friendService.add(friend)
+        .subscribe(newFriend  => {
+          this.friends.push(newFriend);
+        });
+  }
+
+  // toggleComplete(friend: Friend) {
+  //   friend.isComplete = !friend.isComplete;
+  //   this.friendService.update(friend)
+  //       .subscribe(updatedFriend => {
+  //         friend = updatedFriend;
+  //       });
+  // }
+
+  deleteFriend(friend: Friend, index:number) {
+    this.friendService.delete(friend)
+        .subscribe(res => {
+          this.friends.splice(index, 1);
+        });
+  }
+
+  navToEdit(friend: Friend, index: number) {
+    this.nav.push(FriendEditPage, {
+      friend: friend,
+      friends: this.friends,
+      index: index
+    });
   }
 }

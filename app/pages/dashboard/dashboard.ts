d33b7bb50b4273799fb261a9800f6095a6e0184a
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-09-19 17:04:38 -0400

    heroku 7

--- app/pages/dashboard/dashboard.ts
+++ app/pages/dashboard/dashboard.ts
@@ -1,6 +1,9 @@
 import {Component} from "@angular/core";
 import {NavController, NavParams, MenuController} from 'ionic-angular';
 import {WelcomePage} from '../welcome/welcome';
+import {HomePage} from '../home/home';
+import {FriendAddPage} from '../friend-add/friend-add';
+
 import {FriendService} from '../../providers/friend-service/friend-service';
 import {Friend} from '../../friend.ts';
 
@@ -10,6 +13,9 @@ import {Friend} from '../../friend.ts';
 })
 export class DashboardPage {
   // public curruser: NavigatorUserMedia;    // The user itself
+  public friend: Friend;    // The friend itself
+  public friends: Friend[]; // The list of friends from the main page
+  public index: number; // The index of the friend we're looking at
 
   constructor(public friendService: FriendService, public nav: NavController, public navParams: NavParams, public menuCtrl: MenuController ) {
     // this.curruser= navParams.get('friend');
@@ -26,5 +32,19 @@ export class DashboardPage {
   toggleMenu() {
     this.menuCtrl.toggle();
   }
+  navToHome(friend: Friend, index: number) {
+    this.nav.push(HomePage, {
+      friend: friend,
+      friends: this.friends,
+      index: index
+    });
+  }
 
+  navToAdd(friend: Friend, index: number) {
+    this.nav.push(FriendAddPage, {
+      friend: friend,
+      friends: this.friends,
+      index: index
+    });
+  }
 }
--- app/pages/friend-edit/friend-edit.ts
+++ app/pages/friend-edit/friend-edit.ts
@@ -47,4 +47,6 @@ export class FriendEditPage {
       index: index
     });
   }
+
+  
 }

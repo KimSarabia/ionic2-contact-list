10bd4c178cb3895f215a4333f9157c5da5907029
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-09-28 15:54:02 -0400

    round 2 init

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,6 @@ import {ionicBootstrap, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {WelcomePage} from './pages/welcome/welcome';
 import {HomePage} from './pages/home/home';
-import {TabsPage} from './pages/tabs/tabs';
 
 
 
@@ -15,7 +14,7 @@ export class MyApp {
   rootPage: any = WelcomePage;
 
   constructor(platform: Platform) {
-    this.rootPage = TabsPage;
+    // this.rootPage = TabsPage;
 
     platform.ready().then(() => {
       StatusBar.styleDefault();
